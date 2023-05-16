dd22e8e2fccd1384f0d08d57e7ea099d8ca4e5fe
TH1117 <121652394+PRESSANYKEYBOARD@users.noreply.github.com>
2023-05-16 20:25:33 +0900

    20230516 구구단

---
+++ 20230516/Baekjoon_2739.rb
@@ -0,0 +1,10 @@
+i = gets.to_i
+
+for j in 1..9
+    puts "#{i} * #{j} = #{i * j}"
+end
+
+# Ruby에서는 for문을 사용 시... 
+# for 변수 [, 변수 ...] in 표현식 [do]
+#     코드
+# end
---
+++ 20230516/tempCodeRunnerFile.rb
@@ -0,0 +1,5 @@
+i = gets.chomp.to_i
+
+for j in 1..9
+    puts "#{i}" * "#{j}" = "#{i * j}"
+end
